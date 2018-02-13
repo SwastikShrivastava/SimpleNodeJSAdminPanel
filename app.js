9ea770ede5422d2512b8f71b171b8cc9e8a7238f
VellaContributor <shvmvhsshukra@gmail.com>
2018-02-13 19:22:17 +0530

    Balance method added

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var express = require('express'),
     app = express(),
     session = require('express-session');
 
-var path    = require("path");
+var path = require("path");
 var bodyParser = require('body-parser')
+var cors = require('cors')
+var request = require("request");
 
+
+
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -16,8 +22,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
+
+
 var auth = function(req, res, next) {
-  if (req.session && req.session.user == "swastik" && req.session.admin)
+  if (req.session && req.session.user == "codedude" && req.session.admin)
     return next();
   else
     return res.redirect('/login');
@@ -52,7 +60,42 @@ app.post('/login',function(req,res){
   else{
     res.send("Wrong");
   }
-})
+});
+
+
+
+
+app.post('/getBalance',function(req,res){
+
+
+var bal;
+var options = { method: 'POST',
+                      url: 'http://18.220.204.121:9326/acntBalance',
+                      headers: 
+                       { 'postman-token': '297313bf-d5e7-335d-37dc-1edebf764eaf',
+                         'cache-control': 'no-cache',
+                         'content-type': 'application/x-www-form-urlencoded' },
+                      form: 
+                       { apikey: 'ironmandiesininfinityWars',
+                         address: req.body.toaccount } 
+             };
+
+             //console.log(req.body.toaccount);
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  //console.log(JSON.parse(body).tokenBalance);
+  bal = body;
+  res.send(bal)
+
+});
+
+
+
+});
+
+
 
 app.get('/panel',auth,function(req,res){
   res.sendFile(path.join(__dirname+'/panel.html'));
